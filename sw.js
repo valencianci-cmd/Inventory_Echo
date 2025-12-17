35c48db2b85b477e70e89cfbe12d5111472dae2a
valencianci-cmd <valen.cianci@gmail.com>
2025-12-17 09:48:37 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "echo-inventory-v454-pwa-1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req).then((res) => {
+      const copy = res.clone();
+      // Cache same-origin GET requests
+      if (req.method === "GET" && new URL(req.url).origin === self.location.origin) {
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, copy)).catch(()=>{});
+      }
+      return res;
+    }).catch(() => cached))
+  );
+});
